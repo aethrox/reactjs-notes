3d0ef873eb1e69bc27e9bd4ef31fbf296047d59d
1nnr3d <bbod3326@gmail.com>
2021-12-03 13:52:27 +0300

    main

--- props/src/components/User.js
+++ props/src/components/User.js
@@ -5,12 +5,17 @@ class User extends Component {
     render() {
         const { name, department, salary} = this.props;
         return (
-            <div>
-                <ul>
-                    <li>İsim: {name}</li>
-                    <li>Departman: {department}</li>
-                    <li>Maaş: {salary}</li>
-                </ul>
+            <div className="col-md-8 mb-4">
+                <div className="card">
+                    <div className="card-header d-flex justify-content-between">
+                        <h4 className="d-inline">{name}</h4>
+                        <i className="far fa-trash-alt" style={{cursor: 'pointer'}}></i>
+                    </div>
+                    <div className="card-body">
+                        <p className="card-text">Departman: {department}</p>
+                        <p className="card-text">Maaş: {salary}</p>
+                    </div>
+                </div>
             </div>
         )
     }
