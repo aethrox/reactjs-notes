5817ddc86c0a70319d91b2b6e601a3d0efc515d2
1nnr3d <bbod3326@gmail.com>
2021-12-03 13:11:14 +0300

    main

---
+++ props/src/components/User.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class User extends Component {
+    render() {
+        const { name, department, salary} = this.props;
+        return (
+            <div>
+                <ul>
+                    <li>İsim: {name}</li>
+                    <li>Departman: {department}</li>
+                    <li>Maaş: {salary}</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default User;
