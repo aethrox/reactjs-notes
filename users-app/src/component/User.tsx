94a046e9fa1089c53bdaaa6028ae9293861b55a7
1nnr3d <bbod3326@gmail.com>
2021-12-08 20:57:21 +0300

    MAİN

--- users-app/src/component/User.tsx
+++ users-app/src/component/User.tsx
@@ -3,8 +3,23 @@ import React, { Component } from 'react'
 export default class User extends Component {
   render() {
     return (
-      <div>
-        
+      <div className="col-md-8 mb-4">
+        <div className="card">
+          <div className="card-header d-flex justify-content-between">
+            <h4 className="d-inline">{"Test"}</h4>
+            <i
+              // onClick={}
+              className="far fa-trash-alt"
+              style={{ cursor: 'pointer' }}
+            ></i>
+          </div>
+          {
+            <div className="card-body">
+              <p className="card-text">Departman: {}</p>
+              <p className="card-text">Maaş: {}</p>
+            </div>
+          }
+        </div>
       </div>
     )
   }
--- users-app/src/component/Users.tsx
+++ users-app/src/component/Users.tsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import User from './User'
 
 export default class Users extends Component {
   render() {
     return (
       <div>
-        
+        <User />
       </div>
     )
   }
